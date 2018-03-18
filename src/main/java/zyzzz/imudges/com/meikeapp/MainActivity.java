b4df84b4bfdd0e3ae6b280f8273a8fd4f62a2977
Zyzzz <zhouyan@imudges.com>
2018-03-18 14:56:13 +0800

    登录页面

--- src/main/java/zyzzz/imudges/com/meikeapp/MainActivity.java
+++ src/main/java/zyzzz/imudges/com/meikeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package zyzzz.imudges.com.meikeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,16 +14,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+@ContentView(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    @ViewInject(R.id.toolbar)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        x.view().inject(this);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -71,9 +76,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_user) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            Intent intent=new Intent(MainActivity.this, LoginActivity.class); startActivity(intent);
+
+        } else if (id == R.id.nav_course) {
+
 
         }  else if (id == R.id.nav_manage) {
 
